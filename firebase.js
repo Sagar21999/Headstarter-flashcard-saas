82da40fdf3b418a089f5bf7dacfbc45560501b11
Sagar21999 <51937895+Sagar21999@users.noreply.github.com>
2024-08-14 22:38:45 -0400

    initial commit

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+const firebaseConfig = {
+    apiKey: "AIzaSyCquwNZRaQCUH22J_5iY6aYn9qPp9Vo018",
+    authDomain: "headstarter-flashcard-saas.firebaseapp.com",
+    projectId: "headstarter-flashcard-saas",
+    storageBucket: "headstarter-flashcard-saas.appspot.com",
+    messagingSenderId: "1016603248888",
+    appId: "1:1016603248888:web:9ac7aaa99e1705958d930d"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export default db;
